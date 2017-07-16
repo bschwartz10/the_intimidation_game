42328415b0182512fcd0be541b12485bad41d0c7
bschwartz10 <bschwartz92@gmail.com>
2017-07-16 13:20:17 -0600

    not using mysportfeeds api anymore

--- app/controllers/my_sports_feeds_controller.rb
+++
@@ -1,9 +0,0 @@
-class MySportsFeedsController < ApplicationController
-
-  def index
-    msf = MySportsFeeds.new(version="1.0", true)
-    msf.authenticate("bschwartz92", "brettschwartz")
-    data = msf.msf_get_data('nfl', '2015-2016-regular', 'cumulative_player_stats', 'xml', 'team' => 'dallas-cowboys')
-  end
-
-end
