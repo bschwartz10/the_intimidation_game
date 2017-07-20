79850d103941f79c51acb35843162e759d44181e
bschwartz10 <bschwartz92@gmail.com>
2017-07-20 17:52:57 -0600

    change position value to string

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Player, type: :model do
   describe "validations" do
     it "is valid with all valid attributes" do
       team = Team.create(name: "Brett's Team")
-      player = team.players.create(first_name: "Brett", last_name: "Schwartz", position: 0, nfl_team:"Jets")
+      player = team.players.create(first_name: "Brett", last_name: "Schwartz", position: "QB", nfl_team:"Jets")
 
       expect(player).to be_valid
     end
     it "is invalid without a first name" do
       team = Team.create(name: "Brett's Team")
-      player = team.players.create(last_name: "Schwartz", position: 0, nfl_team:"Jets")
+      player = team.players.create(last_name: "Schwartz", position: "QB", nfl_team:"Jets")
 
       expect(player).to_not be_valid
     end
     it "is invalid without a last name" do
       team = Team.create(name: "Brett's Team")
-      player = team.players.create(first_name: "Brett", position: 0, nfl_team:"Jets")
+      player = team.players.create(first_name: "Brett", position: "QB", nfl_team:"Jets")
 
       expect(player).to_not be_valid
     end
@@ -28,12 +28,12 @@ RSpec.describe Player, type: :model do
     end
     it "is invalid without a nfl team" do
       team = Team.create(name: "Brett's Team")
-      player = team.players.create(first_name: "Brett", last_name: "Schwartz", position: 0)
+      player = team.players.create(first_name: "Brett", last_name: "Schwartz", position: "QB")
 
       expect(player).to_not be_valid
     end
     it "is invalid without a team" do
-      player = Player.create(first_name: "Brett", last_name: "Schwartz", position: 0, nfl_team: "Jets")
+      player = Player.create(first_name: "Brett", last_name: "Schwartz", position: "QB", nfl_team: "Jets")
 
       expect(player).to_not be_valid
     end
@@ -41,7 +41,7 @@ RSpec.describe Player, type: :model do
   describe "relationships" do
     it "belongs to a team" do
       team = Team.create(name: "Brett's Team")
-      player = team.players.create(first_name: "Brett", last_name: "Schwartz", position: 0, nfl_team:"Jets")
+      player = team.players.create(first_name: "Brett", last_name: "Schwartz", position: "QB", nfl_team:"Jets")
 
       expect(player).to respond_to(:team)
     end
