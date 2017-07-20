5deb44b1aa62bcc18a0a2d621b89bacde0fe78e7
bschwartz10 <bschwartz92@gmail.com>
2017-07-20 17:52:23 -0600

    create class methods to find players for owned/comp team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,12 @@
 class Team < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :players
+
+  def self.owned_team_players
+    find_by(owned: true).players.order(:position)
+  end
+
+  def self.comp_team_players(id)
+    where(owned: false).find(id).players.order(:position)
+  end
 end
