1b033504853dbe4d35d0d4e9728f9e51c5896be1
bschwartz10 <bschwartz92@gmail.com>
2017-07-20 09:05:10 -0600

    WIP update seed file to seed teams players and stats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+team_names = ['Golden Tate Warriors',
+              'Little Red Fournette',
+              'Hide & Zeke',
+              'Dalvin & The Chipmunks',
+              'When the LeVeon Breaks',
+              'Game of Jones',
+              "Le'Veon la Vida Loca",
+              'Drinkin Fortes',
+              'Forte Shades of Grey',
+              'Julio Let the Dogs Out',
+              'Goff Balls']
+
+Team.create(name: "Bretts Team", owned: true)
+
+11.times do |i|
+  Team.create(name: team_names[i])
+end
+
+CSV.foreach "db/csv/stats.csv", headers: true, header_converters: :symbol do |row|
+  player = Player.new(
+    first_name: row[:name].split(' ')[0],
+    last_name: row[:name].split('\\')[0].split(' ')[1],
+    nfl_team: row[:tm],
+    position: row[:fantpos],
+  )
+  player.save(validate: false)
+
+  player.stats.create!(
+    pass_yards: row[:passyds],
+    pass_tds: row[:passtd],
+    interceptions: row[:int],
+    rush_yards: row[:rushyds],
+    rush_tds: row[:rushtd],
+    receptions: row[:receptions],
+    rec_yards: row[:recyds],
+    rec_tds: row[:rectd],
+    fantasy_points: row[:fantpt] || 0,
+    position_rank: row[:posrank]
+  )
+  puts "#{player.first_name} #{player.last_name} was created with his stats."
+end
+
+teams = Team.all
+
+Player.where(position: "qb").take(12).order(:random).each do |player|
+  team << player
+end
