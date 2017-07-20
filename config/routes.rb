01cfe3f819deba9b3f4f402ca9c79de8f14ac0c9
bschwartz10 <bschwartz92@gmail.com>
2017-07-20 17:52:37 -0600

    create routes for owned/comp team endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'my_sports_feeds#index'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :teams do
+        get '/owned_team_players', to: 'owned_team_players#index'
+        get '/comp_team_players/:id', to: 'comp_team_players#show'
+      end
+    end
+  end
 end
