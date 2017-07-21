bd12a93b276b5283beb932f10657497c6e0816fd
bschwartz10 <bschwartz92@gmail.com>
2017-07-20 19:16:10 -0600

    add routes for comp team and player stats routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
       namespace :teams do
         get '/owned_team_players', to: 'owned_team_players#index'
         get '/comp_team_players/:id', to: 'comp_team_players#show'
+        get '/comp_team', to: 'comp_team#index'
+        get '/comp_team/:id', to: 'comp_team#show'
+      end
+      namespace :players do
+        get '/player_stats/:id', to: 'player_stats#show'
       end
     end
   end
