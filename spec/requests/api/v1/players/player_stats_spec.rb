5fb292be1e003f2b99695262aec67c3c0e1594e4
bschwartz10 <bschwartz92@gmail.com>
2017-07-21 12:03:37 -0600

    change get route in test

--- spec/requests/api/v1/players/player_stats_spec.rb
+++ spec/requests/api/v1/players/player_stats_spec.rb
@@ -6,7 +6,7 @@ describe 'Player stats API' do
     player = team.players.create(first_name: "Aaron", last_name: "Rodgers", position: "QB", nfl_team: "GNB", team_id: team.id)
     player.stats.create(pass_yards: 1000, pass_tds: 10, interceptions: 10, position_rank: 1, fantasy_points: 700)
 
-    get "/api/v1/players/player_stats/#{player.id}"
+    get "/api/v1/players/stats/#{player.id}"
 
     stats = JSON.parse(response.body)
     expect(response).to be_success
