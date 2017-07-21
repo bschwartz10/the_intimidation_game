384cd10b630af96da7413b45244edb2c24190b9e
bschwartz10 <bschwartz92@gmail.com>
2017-07-21 12:03:10 -0600

    modify routes
    only using 3 routes
    teams/teams#index
    teams/teams#show
    players/stats#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       namespace :teams do
-        get '/owned_team_players', to: 'owned_team_players#index'
-        get '/comp_team_players/:id', to: 'comp_team_players#show'
-        get '/comp_team', to: 'comp_team#index'
-        get '/comp_team/:id', to: 'comp_team#show'
+        get '/teams', to: 'teams#index'
+        get '/teams/:id', to: 'teams#show'
       end
       namespace :players do
-        get '/player_stats/:id', to: 'player_stats#show'
+        get '/stats/:id', to: 'stats#show'
       end
     end
   end
