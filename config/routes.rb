95b46e22b5d24933404bbd0e5b3b93f36a2cda1c
bschwartz10 <bschwartz92@gmail.com>
2017-07-25 14:10:42 -0600

    comment out teams routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      namespace :teams do
-        get '/teams', to: 'teams#index'
-        get '/teams/:id', to: 'teams#show'
-      end
+      # namespace :teams do
+      #   get '/teams', to: 'teams#index'
+      #   get '/teams/:id', to: 'teams#show'
+      # end
       namespace :players do
+        get '/stats', to: 'stats#index'
         get '/stats/:id', to: 'stats#show'
       end
     end
