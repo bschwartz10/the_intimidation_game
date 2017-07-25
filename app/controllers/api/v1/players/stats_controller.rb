30720dc75c8612f2035c24f1e9fb42188c8895c5
bschwartz10 <bschwartz92@gmail.com>
2017-07-25 14:10:19 -0600

    create endpoint to render all players and their stats

--- app/controllers/api/v1/players/stats_controller.rb
+++ app/controllers/api/v1/players/stats_controller.rb
@@ -1,5 +1,9 @@
 class::Api::V1::Players::StatsController < ApplicationController
 
+  def index
+    render json: Player.all
+  end
+  
   def show
     render json: Player.find(params[:id]).stats
   end
